6b835feca771c506464adc6de85710e3bce59a2e
JoJo <joevega2020@gmail.com>
2022-06-09 04:11:11 -0500

    Welcome Messaage

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import routes from "././routes/notes.routes.js";
 import db from "./db.js";
 const app = express();
-const PORT = process.env.PORT|| 3050;
+const PORT = process.env.PORT || 3050;
 try {
 	await db.authenticate();
 } catch (error) {
@@ -13,9 +13,19 @@ try {
 //middlesware
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+	res.send({
+		message: "Welcome to the notes API",
+		allNotes: "https://notes-backend-challenge.herokuapp.com/notes",
+		oneNote: "https://notes-backend-challenge.herokuapp.com/notes/1",
+		create: "https://notes-backend-challenge.herokuapp.com/notes/create",
+		update: "https://notes-backend-challenge.herokuapp.com/notes/update/1",
+		delete: "https://notes-backend-challenge.herokuapp.com/notes/delete/1",
+	});
+});
 //routes
 app.use("/notes", routes);
 
 app.listen(PORT, () => {
-	console.log("Server is running on port 4000");
+	console.log("Server is running on port " + PORT);
 });
