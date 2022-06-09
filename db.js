a6473701289d6e0a753d960a5070fd5d3d7fe934
JoJo <joevega2020@gmail.com>
2022-06-09 02:35:23 -0500

    fix connect to database

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 import sequelize from "sequelize";
 //users-password-host-database
 //mysql://b2cf318ac48acb:375384d4@us-cdbr-east-05.cleardb.net/heroku_081e1f7d9406a2d?reconnect=true
+//table = "app_notes"
 const configdb = {
 	user: "b2cf318ac48acb",
 	password: "375384d4",
@@ -9,9 +10,7 @@ const configdb = {
 };
 
 //database, user , password {host}
-const db = new sequelize(configdb.database, configdb.user, configdb.password, {
-	host: configdb.host,
-	dialect: "mysql",
-});
+const db = new sequelize(process.env.DATABASE_URL);
+
 
 export default db;
