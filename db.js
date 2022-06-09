1048d8bdfb0990251113e3b5a2e5ac4b92ef2ecb
JoJo <joevega2020@gmail.com>
2022-06-09 03:34:05 -0500

    Update connection db

--- db.js
+++ db.js
@@ -1,17 +1,8 @@
 import sequelize from "sequelize";
-//users-password-host-database
-//mysql://b2cf318ac48acb:375384d4@us-cdbr-east-05.cleardb.net/heroku_081e1f7d9406a2d?reconnect=true
-//table = "app_notes"
-const configdb = {
-	user: "b2cf318ac48acb",
-	password: "375384d4",
-	host: "us-cdbr-east-05.cleardb.net",
-	database: "heroku_081e1f7d9406a2d",
-};
 
 //database, user , password {host}
 const db = new sequelize(
-	process.env.DB_HOST || "app_notes",
+	process.env.DB_DATABASE || "app_notes",
 	process.env.DB_USER || "root",
 	process.env.DB_PASSWORD || "",
 	{ host: process.env.DB_HOST || "localhost", dialect: "mysql" }
