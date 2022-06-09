7c5ba991d41784a5294f8ea2294ad7d3a76178dc
JoJo <joevega2020@gmail.com>
2022-06-09 03:49:04 -0500

    Update

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const db = new sequelize(
 	process.env.DB_DATABASE || "app_notes",
 	process.env.DB_USER || "root",
 	process.env.DB_PASSWORD || "",
-	{ host: process.env.DB_HOST || "localhost", dialect: "mysql2" }
+	{ host: process.env.DB_HOST || "localhost", dialect: "mariadb" }
 );
 
 export default db;
